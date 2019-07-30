f79b138287e59ea56d40233f41f7d64cd7131b94
William Wyatt <tsangares@gmail.com>
2019-07-30 14:47:42 -0700

    Updated main.py to support flask in its own thread.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, Markup
 from util import getHtml, duplicate, displayArray
 from flask_socketio import SocketIO, emit
+from multiprocessing import Process
+import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
@@ -8,7 +10,6 @@ socketio = SocketIO(app)
 @app.route('/')
 def home():
     return displayArray()
-#    return render_template('hello.html', jsloc, css = displayArray())
 @socketio.on('connect')
 def test_connect():
     emit('after connect',  {'data':'Lets dance'})
@@ -17,9 +18,17 @@ def test_connect():
 #    emit('reload', {'data': message['data']})
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0',port=5001, debug=True)
-    print('a;lsdfkj')
-    while True:
+    def startFlask():
+        socketio.run(app, host='0.0.0.0',port=5001, debug=True)
+    try:
+        flask=Process(target=startFlask)
+        flask.start()
         print('a;lsdfkj')
-        if os.path.getmtime('test.py')<=5:
-            test_message('hello')
+        while True:
+            print('a;lsdfkj')
+            if os.path.getmtime('test.py')<=5:
+                test_message('hello')
+    except KeyboardInterrupt:
+        print("Terminating flask server.")
+        flask.terminate()
+        flask.join()
