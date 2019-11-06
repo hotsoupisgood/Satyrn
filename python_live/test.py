02579ce84403615b45b3a160514599d03e653cac
Lucas <hairyhenry@gmail.com>
2019-11-05 21:54:21 -0800

    Stuff

--- python_live/test.py
+++ python_live/test.py
@@ -1,7 +1,14 @@
 ####
+def hello():
+    print('hello world')
 x=4
 ####
 
+import os
+cwd = os.getcwd()
+print(cwd)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print(dir_path)
 print(x-9)
 
 ####
